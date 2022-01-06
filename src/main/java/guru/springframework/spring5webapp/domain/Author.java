f7b3f4e992ae190efd3466c9d1cb847ce60e5dc1
arpitgupta30 <arpitofficial123@gmail.com>
2022-01-06 21:12:40 +0530

    Added command line interface and extended crud repository

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -18,7 +19,7 @@ public class Author {
 	private String lastName;
 	
 	@ManyToMany(mappedBy = "authors")
-	private Set<Book> books;
+	private Set<Book> books = new HashSet<>();
 	
 	
 
@@ -26,11 +27,10 @@ public class Author {
 		super();
 	}
 
-	public Author(String firstName, String lastName, Set<Book> books) {
+	public Author(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.books = books; 
 	}
 	
 	public Long getId() {
